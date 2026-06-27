67d1ad7cdfa95f8f6836dd9bf1259c1b813dfa8c
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2026-06-27 03:21:34 -0300

    refactor(services): centralize service management and config
    *   refactor cosmos service startup logic
    *   delete cosmos service commands and helper functions
    *   delete unused cosmos config fields and constants
    *   delete gateway query commands and module
    *   change service enum and up function visibility to public

--- interstellar/src/services/cosmos/config.rs
+++ interstellar/src/services/cosmos/config.rs
@@ -2,17 +2,13 @@ use crate::config::{get, ChainId};
 
 pub const COMPOSE_SERVICE: &str = "cosmos";
 
-const DEFAULT_DEVNET_NAME: &str = "cosmos-devnet";
 const DEFAULT_DEVNET_CHAIN_ID: &str = "simd-1";
 const DEFAULT_DEVNET_RPC_URL: &str = "http://127.0.0.1:26657";
 const DEFAULT_DEVNET_REST_URL: &str = "http://127.0.0.1:1317";
-const DEFAULT_DEVNET_GRPC_URL: &str = "http://127.0.0.1:9090";
 
-const DEFAULT_TESTNET_NAME: &str = "cosmos-testnet";
 const DEFAULT_TESTNET_CHAIN_ID: &str = "provider";
 const DEFAULT_TESTNET_RPC_URL: &str = "https://rpc.provider-sentry-01.hub-testnet.polypore.xyz";
 const DEFAULT_TESTNET_REST_URL: &str = "https://rest.provider-sentry-01.hub-testnet.polypore.xyz";
-const DEFAULT_TESTNET_GRPC_URL: &str = "https://grpc.provider-sentry-01.hub-testnet.polypore.xyz";
 const DEFAULT_TESTNET_FAUCET_URL: &str = "https://faucet.polypore.xyz";
 
 const DEFAULT_KEY_NAME: &str = "relayer";
@@ -34,11 +30,9 @@ impl CosmosNetwork {
 
 pub struct CosmosConfig {
     pub network: CosmosNetwork,
-    pub name: String,
     pub chain_id: ChainId,
     pub rpc_url: String,
     pub rest_url: String,
-    pub grpc_url: String,
     pub faucet_url: Option<String>,
     pub key_name: String,
     pub relayer_mnemonic: String,
@@ -53,11 +47,9 @@ impl CosmosConfig {
     pub fn devnet() -> Self {
         Self {
             network: CosmosNetwork::Devnet,
-            name: get("COSMOS_DEVNET_NAME", DEFAULT_DEVNET_NAME),
             chain_id: ChainId::Cosmos(get("COSMOS_CHAIN_ID", DEFAULT_DEVNET_CHAIN_ID)),
             rpc_url: get("COSMOS_RPC_URL", DEFAULT_DEVNET_RPC_URL),
             rest_url: get("COSMOS_REST_URL", DEFAULT_DEVNET_REST_URL),
-            grpc_url: get("COSMOS_GRPC_URL", DEFAULT_DEVNET_GRPC_URL),
             faucet_url: None,
             key_name: get("COSMOS_KEY_NAME", DEFAULT_KEY_NAME),
             relayer_mnemonic: get("COSMOS_RELAYER_MNEMONIC", ""),
@@ -72,11 +64,9 @@ impl CosmosConfig {
     pub fn testnet() -> Self {
         Self {
             network: CosmosNetwork::Testnet,
-            name: get("COSMOS_TESTNET_NAME", DEFAULT_TESTNET_NAME),
             chain_id: ChainId::Cosmos(get("COSMOS_TESTNET_CHAIN_ID", DEFAULT_TESTNET_CHAIN_ID)),
             rpc_url: get("COSMOS_TESTNET_RPC_URL", DEFAULT_TESTNET_RPC_URL),
             rest_url: get("COSMOS_TESTNET_REST_URL", DEFAULT_TESTNET_REST_URL),
-            grpc_url: get("COSMOS_TESTNET_GRPC_URL", DEFAULT_TESTNET_GRPC_URL),
             faucet_url: Some(get("COSMOS_TESTNET_FAUCET_URL", DEFAULT_TESTNET_FAUCET_URL)),
             key_name: get("COSMOS_KEY_NAME", DEFAULT_KEY_NAME),
             relayer_mnemonic: get("COSMOS_RELAYER_MNEMONIC", ""),
@@ -90,13 +80,5 @@ impl CosmosConfig {
 
     pub fn is_local(&self) -> bool {
         self.network.is_local()
-    }
-
-    pub fn status_url(&self) -> String {
-        format!("{}/status", self.rpc_url)
-    }
-
-    pub fn node_info_url(&self) -> String {
-        format!("{}/cosmos/base/tendermint/v1beta1/node_info", self.rest_url)
     }
 }
--- interstellar/src/services/gateway/mod.rs
+++ interstellar/src/services/gateway/mod.rs
@@ -1,8 +1,1 @@
 pub mod config;
-pub mod query;
-
-#[derive(clap::Subcommand)]
-pub enum GatewayCmd {
-    #[command(about = "Direct gateway gRPC reads")]
-    Query,
-}
